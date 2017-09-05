e61cd148b4ca41162d47a5a9ec4642028ce3b3c8
CSer2294365315 <2294365315@qq.com>
2017-09-05 08:55:05 +0800

    import java.awt.*;//////////////////

--- Egg.java
+++ Egg.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.Random;
-
+import java.awt.*;//////////////////
 /**
  * ´ú±íµ°
  * @author Administrator
